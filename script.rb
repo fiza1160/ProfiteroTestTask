87d452c1ce64f1af692d9c280387018d328e596a
Lubov Murzina <46626753+fiza1160@users.noreply.github.com>
2019-04-15 16:38:06 +0300

    Add script and tests

---
+++ script.rb
@@ -0,0 +1,134 @@
+require 'curb'
+require 'nokogiri'
+require 'optimist'
+require 'csv'
+
+def get_args()
+    args = Optimist::options do
+        opt :file_name, "Results file name (full name with path)", :type => :string
+        opt :link, "Link to site category", :type => :string
+    end
+    
+    return args
+end
+
+
+def get_main_pages(html, link)
+    pages = []
+    pages << html
+
+    counter = 1
+    while next_page_exists?(html) do
+        counter += 1
+        http = Curl.get(link, {:p => counter})
+        html = Nokogiri::HTML(http.body_str)
+        pages << html
+    end
+    return pages
+end
+
+
+def parse_main_pages(pages)
+    links = []
+    pages.each do |page|
+        page.xpath("//ul[@id='product_list']//a[@class='product_img_link product-list-category-img']/@href").each do |href|
+            links << href.value
+        end
+    end
+    return links
+end
+
+
+def next_page_exists?(page)
+    if page.xpath("//li[@id='pagination_next_bottom']/@class")[0].value == "disabled pagination_next"
+        return false
+    end
+    return true
+end
+
+def get_product_pages(links)
+    pages = []
+    links.each do |link|
+        http = Curl.get(link)
+        pages << Nokogiri::HTML(http.body_str)
+    end
+    return pages
+end
+
+def parse_product_pages(pages)
+    product_data = {}
+    pages.each do |page|
+
+        main_name = page.xpath("//p[@class='product_main_name']").text.strip.capitalize
+        image = page.xpath("//img[@id='bigpic']/@src")[0].value
+
+        product_list = page.xpath("//div[@class='attribute_list']//li//label")
+        if product_list.length > 0 
+            index = 0
+            product_list.length.times do
+                size = page.xpath("//div[@class='attribute_list']//li//label//span[@class='radio_label']")[index].text.strip
+                prise = page.xpath("//div[@class='attribute_list']//li//label//span[@class='price_comb']")[index].text.strip
+                full_name = "#{main_name} - #{size}"
+                product_data[full_name] = {
+                    :prise => prise,
+                    :image => image
+                }
+                index += 1
+            end
+        else
+            prise = page.xpath("//span[@id='our_price_display']")[0]['content']
+            
+            product_data[main_name] = {
+                :prise => prise,
+                :image => image
+            }
+        end
+    end
+
+    return product_data 
+end
+
+
+def save_in_csv(file_name, data)
+    CSV.open(file_name, "w") do |csv|
+        csv << ["Name", "Prise", "Image"]
+        
+        data.each do |key, val|
+            csv << [key, val[:prise], val[:image]]
+        end
+    end
+end
+
+
+if __FILE__ == $0
+    
+    args = get_args()
+    file_name = args[:file_name]
+    link = args[:link]
+    if link[-1] != '/'
+        link += '/'
+    end
+
+    puts "Download main page..."
+
+    http = Curl.get(link)
+    html = Nokogiri::HTML(http.body_str)
+
+    puts "Expend pagination..."
+    main_pages = get_main_pages(html, link)
+
+    puts "Get links to pages with products..."
+    product_links = parse_main_pages(main_pages)
+
+    puts "Download product pages..."
+    product_pages = get_product_pages(product_links)
+
+    puts "Get information about products..."
+    product_data = parse_product_pages(product_pages)
+
+    puts "Save results to file..."
+    save_in_csv(file_name, product_data)
+
+    puts "Ready! Info about products was saved in #{file_name}"
+
+end
---
+++ tests.rb
@@ -0,0 +1,133 @@
+require "minitest/autorun"
+require "curb"
+require "nokogiri"
+require "/home/fiza/Documents/MyProject/script.rb"
+
+class TestScript < Minitest::Test
+
+  def test_get_main_pages
+    link = "https://www.petsonic.com/hobbit-half/"
+    html = Nokogiri::HTML(Curl.get(link).body_str)
+    result = get_main_pages(html, link)
+
+    assert_equal 3, result.length
+    assert_instance_of Nokogiri::HTML::Document, result[0]
+  end
+
+  def test_parse_main_pages
+    http = Curl.get("https://www.petsonic.com/hobbit-half/")
+    pages = [Nokogiri::HTML(http.body_str)]
+
+    result = [
+      "https://www.petsonic.com/hobbit-alf-barritas-redonda-ternera-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-barritas-redonda-cordero-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-barritas-redonda-pollo-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-mini-huesitos-tiernos-mix-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-hueso-piel-sabor-barbacoa-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-hueso-piel-sabor-chocolate-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-hueso-piel-fluor-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-nudillo-ternera-calcio-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-nudillo-ternera-ahumado-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-canailla-jamon-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-hueso-jamon-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-hueso-calcio-corto-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-hueso-ternera-calcio-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-rosco-piel-fluor-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-hueso-ternera-ahumado-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-orejas-cerda-extra-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-oreja-vaca-deshidratada-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-pezuna-ternera-rellena-para-perros.html",
+      "https://www.petsonic.com/trixie-zapato-piel-masticable-10-unidades-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-oreja-cerdo-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-tendon-cuero-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-puntas-nervio-toro-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-tendon-extra-20cm-para-perros.html",
+      "https://www.petsonic.com/hobbit-alf-tira-tendon-para-perros.html"
+    ]
+
+    assert_equal result, parse_main_pages(pages)
+
+  end
+
+  def test_get_product_pages
+    links = ["https://www.petsonic.com/hobbit-alf-tira-tendon-para-perros.html"]
+
+    assert_instance_of Nokogiri::HTML::Document, get_product_pages(links)[0]
+  end
+
+  def test_parse_product_pages_three_products
+
+    http = Curl.get("https://www.petsonic.com/hobbit-alf-galletas-puppy-para-perros.html")
+    pages = [Nokogiri::HTML(http.body_str)]
+
+    result = {
+        "Hobbit alf galletas puppy para perro - 100 Gr." => {
+            :prise => "0.89 €/u",
+            :image => "https://img1.petsonic.com/14110-large_default/hobbit-alf-galletas-puppy-para-perros.jpg"
+        },
+        "Hobbit alf galletas puppy para perro - 200 Gr." => {
+          :prise => "1.79 €/u",
+          :image => "https://img1.petsonic.com/14110-large_default/hobbit-alf-galletas-puppy-para-perros.jpg"
+        },
+        "Hobbit alf galletas puppy para perro - 430 gr" => {
+          :prise => "2.84 €/u",
+          :image => "https://img1.petsonic.com/14110-large_default/hobbit-alf-galletas-puppy-para-perros.jpg"
+        }
+    }
+    assert_equal result, parse_product_pages(pages)
+  end
+
+  def test_parse_product_pages_one_product
+
+    http = Curl.get("https://www.petsonic.com/hobbit-alf-muslitos-pollo-calcio-para-perros.html")
+    pages = [Nokogiri::HTML(http.body_str)]
+
+    result = {
+        "Hobbit alf muslitos de pollo y calcio para perro - 100 Gr." => {
+            :prise => "2.49 €/u",
+            :image => "https://img3.petsonic.com/11056-large_default/hobbit-alf-muslitos-pollo-calcio-para-perros.jpg"
+        },
+    }
+    assert_equal result, parse_product_pages(pages)
+  end
+
+  def test_save_in_csv
+
+    file_name = "test_data.csv"
+    data = {
+      "Hobbit alf galletas puppy para perro - 100 Gr." => {
+          :prise => "0.89 €/u",
+          :image => "https://img1.petsonic.com/14110-large_default/hobbit-alf-galletas-puppy-para-perros.jpg"
+      },
+      "Hobbit alf galletas puppy para perro - 200 Gr." => {
+        :prise => "1.79 €/u",
+        :image => "https://img1.petsonic.com/14110-large_default/hobbit-alf-galletas-puppy-para-perros.jpg"
+      },
+      "Hobbit alf galletas puppy para perro - 430 gr" => {
+        :prise => "2.84 €/u",
+        :image => "https://img1.petsonic.com/14110-large_default/hobbit-alf-galletas-puppy-para-perros.jpg"
+      }
+    }
+
+    save_in_csv(file_name, data)
+
+    result = CSV.read(file_name)
+    
+    expected_result = [
+      ["Name", "Prise", "Image"], 
+      ["Hobbit alf galletas puppy para perro - 100 Gr.", 
+        "0.89 €/u", 
+        "https://img1.petsonic.com/14110-large_default/hobbit-alf-galletas-puppy-para-perros.jpg"], 
+      ["Hobbit alf galletas puppy para perro - 200 Gr.", 
+        "1.79 €/u", 
+        "https://img1.petsonic.com/14110-large_default/hobbit-alf-galletas-puppy-para-perros.jpg"], 
+      ["Hobbit alf galletas puppy para perro - 430 gr", 
+        "2.84 €/u", 
+        "https://img1.petsonic.com/14110-large_default/hobbit-alf-galletas-puppy-para-perros.jpg"]
+      ]
+
+    assert_equal result, expected_result
+
+  end
+
+end
